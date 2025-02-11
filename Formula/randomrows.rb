ebd0c7a4c69f1d4bf316d7fd60521fad4f3f0c77
Neal Caren <neal.caren@gmail.com>
2025-02-11 10:28:16 -0500

    Update randomrows to version 1.1

--- Formula/randomrows.rb
+++ Formula/randomrows.rb
@@ -1,8 +1,8 @@
 class Randomrows < Formula
   desc "Display the header and random rows from a CSV or text file"
   homepage "https://github.com/nealcaren/homebrew-tools"
-  url "https://github.com/nealcaren/homebrew-tools/archive/refs/tags/v1.0.tar.gz"
-  sha256 "a4602d5a7957af6da47dbc777a860a2af49c6d38e8433d255a58cdbdf125bd8b"
+  url "https://github.com/nealcaren/homebrew-tools/archive/refs/tags/v1.1.tar.gz"
+  sha256 "01f8518ce2492b86ebdd20530c5d99c8afad8506e24392d6aced8062c22bc0ca"
   version "1.0"
 
   def install
