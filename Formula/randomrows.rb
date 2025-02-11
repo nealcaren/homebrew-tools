dd6afe38ac01198e526932d81c03fe352192d58b
Neal Caren <neal.caren@gmail.com>
2025-02-11 08:34:09 -0500

    New Hash just dropped

--- Formula/randomrows.rb
+++ Formula/randomrows.rb
@@ -2,7 +2,7 @@ class Randomrows < Formula
   desc "Display the header and random rows from a CSV or text file"
   homepage "https://github.com/nealcaren/homebrew-tools"
   url "https://github.com/nealcaren/homebrew-tools/archive/refs/tags/v1.0.tar.gz"
-  sha256 "GENERATED_SHA256_HASH"
+  sha256 "a4602d5a7957af6da47dbc777a860a2af49c6d38e8433d255a58cdbdf125bd8b"
   version "1.0"
 
   def install
