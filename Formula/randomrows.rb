5c1552a064fea96bbce9b3c72e510c5fcd7a5460
Neal Caren <neal.caren@gmail.com>
2025-02-11 08:31:16 -0500

    Add formula

---
+++ Formula/randomrows.rb
@@ -0,0 +1,11 @@
+class Randomrows < Formula
+  desc "Display the header and random rows from a CSV or text file"
+  homepage "https://github.com/nealcaren/homebrew-tools"
+  url "https://github.com/nealcaren/homebrew-tools/archive/refs/tags/v1.0.tar.gz"
+  sha256 "GENERATED_SHA256_HASH"
+  version "1.0"
+
+  def install
+    bin.install "bin/randomrows"
+  end
+end
