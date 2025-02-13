7eb1465e3c6230a81fca4b6fe2e30c2ce459e892
Neal Caren <neal.caren@gmail.com>
2025-02-13 09:28:19 -0500

    Mabye?

--- Formula/randomrows.rb
+++ Formula/randomrows.rb
@@ -1,9 +1,9 @@
 class Randomrows < Formula
   desc "Display the header and random rows from a CSV or text file"
   homepage "https://github.com/nealcaren/homebrew-randomrows"
-  url "https://github.com/nealcaren/homebrew-randomrows/archive/v1.3.tar.gz"
+  url "https://github.com/nealcaren/homebrew-randomrows/archive/refs/tags/v1.3.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  version "1.3.0"
+  version "1.3"
   head "https://github.com/nealcaren/homebrew-randomrows.git", branch: "main"
 
   def install
