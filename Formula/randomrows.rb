f4c7880a1d0eb353209a7c9103e71e639ad8290d
Neal Caren <neal.caren@gmail.com>
2025-02-15 09:34:46 -0500

    New sha256

--- Formula/randomrows.rb
+++ Formula/randomrows.rb
@@ -2,7 +2,7 @@ class Randomrows < Formula
   desc "Display the header and random rows from a CSV or text file"
   homepage "https://github.com/nealcaren/randomrows"
   url "https://github.com/nealcaren/randomrows/archive/refs/tags/v1.3.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "928ac1996c1074fe2894a792c7fe892f17ce1e293f974cf952e9c2e70df33ebf"
   version "1.3"
   head "https://github.com/nealcaren/randomrows.git", branch: "main"
 
